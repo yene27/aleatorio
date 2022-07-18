4e391c5c3b523265b2916c8dbf406b2a53073789
yene27 <yene27@yahoo.com>
2022-07-18 07:25:51 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    aleatorio = randint(1, 45)
+    basic.showNumber(aleatorio)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(aleatorio)
+})
+let aleatorio = 0
+aleatorio = 0
